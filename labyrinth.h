5476846d3077cfd4260d2182ab5f5289758c4885
MartinianoOjeda <martiniano_ojeda@hotmail.com>
2022-08-12 13:06:09 -0300

    creando espacio

--- labyrinth.h
+++ labyrinth.h
@@ -6,7 +6,9 @@
 #include <time.h>
 
 #define ESC 27
-#define MAX_RANGE 10
+#define RANDOM_NUM_RANGE 11
+#define X_SIZE 50
+#define Y_SIZE 20
 
 //PARTS
 #define VERT_LINE_INTER_MID_LEFT 185
@@ -21,12 +23,34 @@
 #define HORI_LINE_INTER_MID_DOWN 203
 #define CROSSING 206
 
+#define EMPTY 32
+#define COLOR 34
+
+#define LEFT_UC_X 0 
+#define LEFT_UC_Y 0
+#define LEFT_LC_X 0
+#define LEFT_LC_Y Y_SIZE - 1
+#define RIGHT_UC_X X_SIZE - 1
+#define RIGHT_UC_Y 0
+#define RIGHT_LC_X X_SIZE - 1
+#define RIGHT_LC_Y Y_SIZE - 1
+
+typedef int labyrinth[X_SIZE][Y_SIZE];
+
+typedef struct laby{
+    labyrinth matix_lab;
+}lab;
 
 //PROTOTIPADO
 void hideCursor();
 void gotoxy(int, int);
 void gotoXY(int, int);
 void print(int, int, int, int, char);
+lab createBorder(lab);    
+    lab initializeArray(lab);
+    lab edges(lab);
+    lab corners(lab);
+void printArray(int, int, lab);
 
 
 void hideCursor() {
@@ -36,15 +60,6 @@ void hideCursor() {
     cci.dwSize = 100;
     cci.bVisible = FALSE;
     SetConsoleCursorInfo(hc, &cci); 
-}
-
-void gotoxy(int x, int y) {
-    HANDLE hc;
-    COORD cordXY;
-    cordXY.X = x;
-    cordXY.Y = y;
-
-    SetConsoleCursorPosition(hc, cordXY);
 }
 
 void gotoXY(int x, int y) {
@@ -58,12 +73,72 @@ void printXY(int x, int y, int color, int tone, int c) {
     printf("%c[0m", ESC); //reestablece el color por defecto de la consola
 }
 
-int randomNumber() {
-    
+int randomNumber() {    
     int n = 0;
     srand(time(NULL));
     //srand((unsigned) time(&t));
-    n =  rand() % MAX_RANGE;
-    
+    n =  rand() % RANDOM_NUM_RANGE;
     return n + 1;
 }
+
+lab initializeArray(lab l) {
+    int i, j;
+    for(i = 0; i < X_SIZE; i++) {
+        for(j = 0; j < Y_SIZE; j++) {
+            l.matix_lab[i][j] == EMPTY;
+        }
+    }
+
+    return l;
+}
+
+lab edges(lab l) {
+    int i, j;
+    for(i = 0; i < X_SIZE; i++) {
+        for(j = 0; j < Y_SIZE; j++) {
+            if((j == 0 || j == Y_SIZE - 1) && (i > 0 && i < X_SIZE - 1)) {
+                l.matix_lab[i][j] = HORI_LINE;
+            }
+            else {
+                if ((i == 0 || i == X_SIZE - 1) && (j > 0 && j < Y_SIZE - 1)) {
+                    l.matix_lab[i][j] = VERT_LINE;
+                    if(j == (Y_SIZE/2)) {
+                        l.matix_lab[i][j] = EMPTY;
+                    }
+                }
+                else {
+                    l.matix_lab[i][j] = EMPTY;
+                }
+            }
+        }
+    }
+
+    return l;
+}
+
+lab corners(lab l) {
+    l.matix_lab[LEFT_UC_X][LEFT_UC_Y] = LEFT_UPPER_CORNER;
+    l.matix_lab[LEFT_LC_X][LEFT_LC_Y] = LEFT_LOWER_CORNER;
+    l.matix_lab[RIGHT_UC_X][RIGHT_UC_Y] = RIGHT_UPPER_CORNER;
+    l.matix_lab[RIGHT_LC_X][RIGHT_LC_Y] = RIGHT_LOWER_CORNER;
+    return l;
+}
+
+lab createBorders(lab l) {
+    l = initializeArray(l);
+    l = edges(l);
+    l = corners(l);
+    return l;
+}
+
+void printArray(int x, int y, lab l) {
+    int i, j;
+    for(i = 0; i < X_SIZE; i++) {
+        for(j = 0; j < Y_SIZE; j++) {
+            if(l.matix_lab[i][j] != EMPTY) {
+                printXY(x+i, y+j, COLOR, 1, l.matix_lab[i][j]);
+            }
+        }
+    }
+}
+
--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include "labyrinth.h"
 
+#define X_DIS 2
+#define Y_DIS (X_DIS/2)
+
+
 int main() {
-    int key;
-    int game_over;
+    int x, y, key, game_over;
+    lab l;
+    game_over = 0;
 
     system("cls");
+
+    hideCursor();
+
+    l = createBorders(l);
+    printArray(X_DIS, Y_DIS, l);
 
     while(!game_over) {
         key = 0;
